33e2e6166d5db06504174b17cf8cfce9d1783c86
Shakhawat Hossain <mdshakwat.hossain@gmail.com>
2023-06-04 22:44:26 +0600

    add to cart commit two

--- context/StateContext.jsx
+++ context/StateContext.jsx
@@ -9,9 +9,13 @@ const Context=createContext();
 export const StateContext=({children})=>{
         const [showCart, setShowCart]=useState(false);
         const[cartItems, setCartItems]=useState([]);
-        const[totalPrice, setTotalPrice]=useState();
+        const[totalPrice, setTotalPrice]=useState(0);
         const[totalQuantities, setTotalQuantities]=useState(0);
         const[qty, setQty]=useState(1);
+
+
+        let foundProduct;
+        let index;
 
         const onAdd=(product, quantity)=>{
             const checkProductInCart=cartItems.find((item)=>item._id===product._id);
@@ -36,6 +40,22 @@ export const StateContext=({children})=>{
             }
             toast.success(`${qty} ${product.name} added to the cart.`);
 
+        }
+
+
+        const toggleCartItemQuantity=(id,value)=>{
+            foundProduct=cartItems.find((item)=>item._id===id)
+            index=cartItems.findIndex((product)=>product._id===id)
+
+
+            if (value==='inc'){
+                let newCartItems=[...cartItems, {...product,quantity:
+                product.quantity + 1}]
+                 setCartItems(newCartItems)
+
+            } else if(value==='dec'){
+
+            }
         }
 
 
