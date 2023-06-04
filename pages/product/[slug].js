ddd27f23b659e7bc664dd2999b22109a12c3d074
Shakhawat Hossain <mdshakwat.hossain@gmail.com>
2023-06-04 18:17:37 +0600

    add to cart commit one

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -11,7 +11,7 @@ import {useStateContext} from '../../context/StateContext';
 const ProductDetails = ({product,products}) => {
    const{image,name,details,price}=product;
    const [index, setIndex]=useState(0);
-   const {decQty, inQty, qty}=useStateContext();
+   const {decQty, inQty, qty, onAdd}=useStateContext();
   return (
   <div>
       <div className="product-detail-container">
@@ -57,7 +57,7 @@ const ProductDetails = ({product,products}) => {
             </p>
           </div>
           <div className="buttons">
-            <button type="button" className="add-to-cart" onClick="">Add to Cart</button>
+            <button type="button" className="add-to-cart" onClick={()=>onAdd(product,qty)}>Add to Cart</button>
             <button type="button" className="buy-now" onClick="">Buy Now</button>
           </div>
         </div>
