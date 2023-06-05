d114e612d08c45d53b3badc08f06c7e0f4aaf8e1
Shakhawat Hossain <mdshakwat.hossain@gmail.com>
2023-06-05 23:13:04 +0600

    payment method error commit

--- pages/api/stripe.jsx
+++ pages/api/stripe.jsx
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_options: [
-          { shipping_rate: 'shr_1NFeqvHXxHHqqBSEtq1eoK3N' },
+          { shipping_rate: 'shr_1NFg4cHXxHHqqBSE5ma7ZgrZ' },
         ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
